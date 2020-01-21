91b195896c09ad5b898c964f02b342771c5c7e0d
Abdulrahman Y. idlbi <adlogi@gmail.com>
2020-01-22 01:25:29 +0300

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let h = parseInt(time.substring(0, 2));
+  if (h < 12) return 'Good Morning';
+  if (h < 17) return 'Good Afternoon';
+  return 'Good Evening';
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(str) {
+  document.getElementById('greeting').innerText = str;
+}
