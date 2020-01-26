3ce769564e2c1c3c8a49f63f7d49c1e215ea710b
Mavera <mkaymakci.sm@gmail.com>
2020-01-26 17:46:33 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let hour = parseInt(time);
+  if (hour < 12) {
+    return "Good Morning";
+  }
+  if (hour >= 12 && hour < 17) {
+    return "Good Afternoon";
+  }
+
+  if (hour >= 17) {
+    return "Good Evening";
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(pp) {
+  document.getElementById("greeting").innerText = pp;
+}
