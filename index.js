d37a870972af4664a7c90883a7a7dde65f4505a3
eliftabak <elifchorghay@gmail.com>
2020-04-08 20:07:27 +0300

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
-
 /* Write your implementation of greet() */
+function greet(input) {
+  const numberTime = parseInt(input);
+  if(numberTime<12) {
+    return `Good Morning`
+  } else
+  if (numberTime>=12 && numberTime<=17) {
+    return `Good Afternoon`
+  }else  {
+    return `Good Evening`
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+const heading = document.querySelector("#greeting").innerText = message;
+}
