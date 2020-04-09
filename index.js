4ea841e1acd802565443415122f555a1fb68957e
Refia Candan Ozkalay <refia.candan@gmail.com>
2020-04-09 11:13:50 +0300

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,27 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(timeString) {
+  let timeArray = timeString.split(":"); //=> ['HH', 'MM']
+  let hourString = timeArray[0];
+  let hour = parseInt(hourString, 10);
+  
+  if (hour < 12) {
+    return "Good Morning";
+  } 
+  else if (hour < 17) {
+    return "Good Afternoon";
+  }
+  else { 
+    return "Good Evening";
+  }
+
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message) {
+  let timeText = document.getElementById("greeting");
+  timeText.innerText = message;
+}
