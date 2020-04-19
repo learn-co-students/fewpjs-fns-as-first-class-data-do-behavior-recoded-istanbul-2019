405378d0fb7c88dda75aecc72180ee3ab95f40db
kamalbakour <kamal.bak92@gmail.com>
2020-04-19 14:52:52 +0300

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
+function greet(timeString){
+  let time = parseInt(timeString);
+
+   if (time < 12) {
+       return "Good Morning";
+   } else if (time >= 12 && time <= 17) {
+       return "Good Afternoon";
+   } else {
+       return "Good Evening";
+   }
+   
+}
+function displayMessage(timeString) {
+
+  document.getElementById("greeting").innerText = timeString;
+}
+//let result = displayMessage(timeString);
 
 /* End Given Code, don't edit above here...*/
 
